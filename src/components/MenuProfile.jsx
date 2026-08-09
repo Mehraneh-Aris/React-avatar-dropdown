dfd71b96454a8f3e9c30a1847a72ac451b1680fb
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-09 19:19:15 +0330

    Build initial component structure

--- src/components/MenuProfile.jsx
+++ src/components/MenuProfile.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import personImg from "../pictures/—Pngtree—a young waitress is posing_15501995.png";
+import "../styles/MenuProfile.css"
 
 const MenuProfile = () => {
     return (
-        <> 
-        
-        </>
+        <div className="profile-menu-container">
+            <div className="avatar">
+                <img src={personImg} alt="profile user" />
+            </div>
+
+            <div className="menu-dropdown">
+                <div className="menu-header">
+                    <strong>Person name</strong>
+                    <small>persongmail@gmail.com</small>
+                </div>
+                <ul>
+                    <li>My profile</li>
+                    <li>setting</li>
+                    <li>log out</li>
+                </ul>
+            </div>
+        </div>
     );
 
 }
