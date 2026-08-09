c874fa82eb012e6c088290847f5864e8d0ca5526
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-09 18:48:03 +0330

    Set up React project structure

---
+++ src/components/MenuProfile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MenuProfile = () => {
+    return (
+        <> <p>hi</p></>
+    );
+
+}
+
+export default MenuProfile;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuProfile from "../components/MenuProfile";
+
+const Home = () => {
+    return (
+        <>
+            <MenuProfile/>
+        </>
+    );
+
+}
+
+export default Home;
