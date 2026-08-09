fc7640c395adfeee35c475cbfeb219f31e797ab8
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-09 20:30:39 +0330

    add avatar manu toggle functionality

--- src/components/MenuProfile.jsx
+++ src/components/MenuProfile.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import personImg from "../pictures/—Pngtree—a young waitress is posing_15501995.png";
 import "../styles/MenuProfile.css"
 
 const MenuProfile = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const handleToggle = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <div className="profile-menu-container">
-            <div className="avatar">
+            <div className="avatar" onClick={handleToggle}>
                 <img src={personImg} alt="profile user" />
             </div>
 
-            <div className="menu-dropdown">
-                <div className="menu-header">
-                    <strong>Person name</strong>
-                    <small>persongmail@gmail.com</small>
+            {isOpen && (
+                <div className="menu-dropdown">
+                    <div className="menu-header">
+                        <strong>Person name</strong>
+                        <small>persongmail@gmail.com</small>
+                    </div>
+                    <ul>
+                        <li>My profile</li>
+                        <li>setting</li>
+                        <li>log out</li>
+                    </ul>
                 </div>
-                <ul>
-                    <li>My profile</li>
-                    <li>setting</li>
-                    <li>log out</li>
-                </ul>
-            </div>
+            )}
+
         </div>
     );
 
